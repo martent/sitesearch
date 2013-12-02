e81fdb5e4e8e31fe2b86747752ea6ac723f766b7
martent <marten@thavenius.se>
2013-12-02 14:09:11 +0100

    Capistrano deployment setup

---
+++ config/deploy.rb
@@ -0,0 +1,56 @@
+set :application, 'sitesearch'
+set :repo_url, "git@github.com:malmostad/#{application}.git"
+
+set :branch, "master"
+
+set :user, "deployer"
+set :deploy_to, "/home/#{user}/apps/#{application}"
+set :scm, :git
+set :deploy_via, :remote_cache
+# set :use_sudo, false
+
+# set :format, :pretty
+# set :log_level, :debug
+set :pty, true
+set :forward_agent, true
+
+# set :linked_files, %w{config/database.yml}
+# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+set :keep_releases, 5
+
+namespace :deploy do
+  %w[start stop restart].each do |command|
+    desc "#{command} unicorn server"
+    task command, roles: :app, except: {no_release: true} do
+      run "/etc/init.d/unicorn_#{application} #{command}"
+    end
+  end
+
+  task :setup_config, roles: :app do
+    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+    sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
+    run "mkdir -p #{shared_path}/config"
+    put File.read("config/settings.example.yml"), "#{shared_path}/config/settings.yml"
+    puts "Now edit the config files in #{shared_path}."
+  end
+  after "deploy:setup", "deploy:setup_config"
+
+  task :symlink_config, roles: :app do
+    run "ln -nfs #{shared_path}/config/settings.yml #{release_path}/config/settings.yml"
+  end
+  after "deploy:finalize_update", "deploy:symlink_config"
+
+  desc "Make sure local git is in sync with remote."
+  task :check_revision, roles: :web do
+    unless `git rev-parse HEAD` == `git rev-parse origin/master`
+      puts "WARNING: HEAD is not the same as origin/master"
+      puts "Run `git push` to sync changes."
+      exit
+    end
+  end
+  before "deploy", "deploy:check_revision"
+
+  after :finishing, 'deploy:cleanup'
+end
