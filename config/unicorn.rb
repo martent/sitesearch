dfcdca5a4a65bc636a521bd7d4e1efd26bfa0951
martent <marten@thavenius.se>
2013-12-03 16:34:35 +0100

    Unicorn fine-tuning

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,7 +4,7 @@ pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
-listen "/tmp/unicorn.blog.sock"
+listen "/tmp/unicorn.sitesearch.sock"
 worker_processes 2
 timeout 15
-# preload_app true
+preload_app true
