15582e358f47bfb282e9ea44fb277c0c34828562
martent <marten@thavenius.se>
2014-03-05 10:39:08 +0100

    Moved views and assets from Sinatra app

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
+  def title(t)
+    @title = t
+  end
+
+  def title_suffix
+    APP_CONFIG["title_suffix"]
+  end
+
+  def h1
+    @title
+  end
+
+  def page_title
+    !@title.nil? ? "#{@title} - #{title_suffix}" : title_suffix
+  end
+
+  def delete_icon_text
+    raw "#{content_tag(:span, nil, class: 'icon-trash icon-large')} Radera"
+  end
+
+  def add_icon_text
+    raw "#{content_tag(:span, nil, class: 'icon-plus')} Lägg till"
+  end
 end
