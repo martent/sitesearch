29aca9f4d17204279c1c68c238d1651ab8f79d2a
martent <marten@thavenius.se>
2014-03-05 16:47:26 +0100

    Config

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,7 @@ require File.expand_path('../boot', __FILE__)
 require "active_model/railtie"
 require "active_record/railtie"
 require "action_controller/railtie"
-require "action_mailer/railtie"
+# require "action_mailer/railtie"
 require "action_view/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
@@ -21,10 +21,17 @@ module Sitesearch
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Stockholm'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = "sv-SE"
+    config.encoding = "utf-8"
+    config.filter_parameters += [:password]
+
+    # config.generators do |g|
+    #   g.template_engine :haml
+    #   g.test_framework :rspec
+    # end
   end
 end
