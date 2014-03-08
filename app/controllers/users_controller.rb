b8b096dec5325bb10e1a4c1ecb55ba07c61fb61f
martent <marten@thavenius.se>
2014-03-08 19:33:39 +0100

    Changed strong params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,6 @@ class UsersController < ApplicationController
 
   private
     def user_params
-      params.require(:user).permit(:username, :password, :password_confirmation, :password_digest)
+      params.require(:user).permit(:username, :password)
     end
 end
