49bb9f30a3119ff98eddc15bd9a70a621316d263
martent <marten@thavenius.se>
2014-03-08 19:41:17 +0100

    DB stored sessions

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'rails', '4.1.0.beta2'
+gem 'activerecord-session_store', '~> 0.1.0'
 gem 'haml-rails'
 gem 'sass-rails', '~> 4.0.1'
 gem 'uglifier', '>= 1.3.0'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140308170935) do
+ActiveRecord::Schema.define(version: 20140308183646) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,16 @@ ActiveRecord::Schema.define(version: 20140308170935) do
   end
 
   add_index "recommendations", ["name"], name: "index_recommendations_on_name", unique: true, using: :btree
+
+  create_table "sessions", force: true do |t|
+    t.string   "session_id", null: false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], name: "index_sessions_on_session_id", unique: true, using: :btree
+  add_index "sessions", ["updated_at"], name: "index_sessions_on_updated_at", using: :btree
 
   create_table "terms", force: true do |t|
     t.string   "name"
