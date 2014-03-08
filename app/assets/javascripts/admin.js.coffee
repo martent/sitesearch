59db70aaf5028c0c210cfde1fb22bee8b8e6da1e
martent <marten@thavenius.se>
2014-03-08 13:48:02 +0100

    Dynamically add term fields to form

--- app/assets/javascripts/admin.js.coffee
+++ app/assets/javascripts/admin.js.coffee
@@ -3,3 +3,9 @@ $ ->
     event.preventDefault()
     $(@).closest(".controls").find("input[type=hidden]").val(true)
     $(@).closest(".control-group").hide()
+
+  $("#edit-recommendation").on "click", ".add-term", (event) ->
+    event.preventDefault()
+    time = new Date().getTime()
+    regexp = new RegExp($(@).data('id'), 'g')
+    $(@).closest(".control-group").before($(@).data('fields').replace(regexp, time))
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,6 +15,15 @@ module ApplicationHelper
     !@title.nil? ? "#{@title} - #{title_suffix}" : title_suffix
   end
 
+  def link_to_add_terms(name, form)
+    new_term = Term.new
+    id = new_term.object_id
+    fields = form.simple_fields_for(:terms, new_term, child_index: id) do |t|
+      render("fields_for_terms", t: t)
+    end
+    link_to(name, '#', class: "btn add-term", data: {id: id, fields: fields.gsub("\n", "")})
+  end
+
   def separator(current, total, question=false)
     separator = ", " if total > current + 2
     separator = " eller " if total == current + 2
