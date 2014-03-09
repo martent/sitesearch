7fbb4805bdfb7981123a8c5e5df082e5a6ec6ec5
martent <marten@thavenius.se>
2014-03-09 21:54:41 +0100

    Cleaned up user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,7 @@
 class UsersController < ApplicationController
-  before_action :authorize, except: [:new, :create]
+  before_action :authorize
 
-  def new
-    @user = User.new
+  def index
+    @users = User.all
   end
-
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to root_url, notice: "Thank you for signing up!"
-    else
-      render "new"
-    end
-  end
-
-  private
-    def user_params
-      params.require(:user).permit(:username, :password)
-    end
 end
