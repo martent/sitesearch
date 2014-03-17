fd62b4e2987d034836b34b374d3b3bd4605e4cfc
martent <marten@thavenius.se>
2014-03-17 10:47:19 +0100

    Image scaling fix

--- app/uploaders/recommendation_image_uploader.rb
+++ app/uploaders/recommendation_image_uploader.rb
@@ -1,11 +1,7 @@
 class RecommendationImageUploader < CarrierWave::Uploader::Base
-  # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
-
-  # Choose what kind of storage to use for this uploader:
   storage :file
-  # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -13,29 +9,17 @@ class RecommendationImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url
-    # For Rails 3.1+ asset pipeline compatibility:
     ActionController::Base.helpers.asset_path("info.png")
   end
 
-  # Process files as they are uploaded:
-  # process :scale => [200, 300]
-  #
-  # def scale(width, height)
-  #   # do something
-  # end
-
-  # Create different versions of your uploaded files:
   version :mini do
-    process resize_to_fit: [32, 32]
+    process resize_to_limit: [32, 64]
   end
   version :thumb do
-    process resize_to_fit: [100, 100]
+    process resize_to_limit: [100, 200]
   end
 
-  # Add a white list of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
   def extension_white_list
     %w(jpg jpeg gif png)
   end
