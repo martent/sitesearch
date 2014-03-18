6ca47587ff77360285313e50476909e47fe20eaa
martent <marten@thavenius.se>
2014-03-18 11:19:29 +0100

    Symbols to strings

--- spec/support/omniauth_macros.rb
+++ spec/support/omniauth_macros.rb
@@ -3,16 +3,16 @@ module OmniauthMacros
     # The mock_auth configuration allows you to set per-provider (or default)
     # authentication hashes to return during integration testing.
     OmniAuth.config.mock_auth[:github] = {
-      provider: "github",
-      uid: "user-1",
-      info: {
-        name: "First Last",
-        nickname: "user-1",
-        email: "first.last@example.org"
+      "provider" => "github",
+      "uid" => "user-1",
+      "info" => {
+        "name" => "First Last",
+        "nickname" => "user-1",
+        "email" => "first.last@example.org"
       },
-      extra: {
-        raw_info: {
-          organizations_url: "http://example.org"
+      "extra" => {
+        "raw_info" => {
+          "organizations_url" => "http://example.org"
         }
       }
     }
