923601cdcb7b189e3a8db99938724dbfcd6ed355
martent <marten@thavenius.se>
2014-03-20 10:41:06 +0100

    Bug fix in oauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,6 +3,6 @@ OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github,
       APP_CONFIG['omniauth']['github_key'],
-      APP_CONFIG['omniauth']['github_key'],
+      APP_CONFIG['omniauth']['github_secret'],
       scope: "read:org,user:email"
 end
