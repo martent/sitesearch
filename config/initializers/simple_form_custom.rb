a106b2501254f94f1a009cc9f3f594442ad938b7
martent <marten@thavenius.se>
2014-05-04 17:30:14 +0200

    Added custom form wrapper for 'basic'

---
+++ config/initializers/simple_form_custom.rb
@@ -0,0 +1,13 @@
+SimpleForm.setup do |config|
+  config.wrappers :basic, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
+    b.use :html5
+    b.use :placeholder
+    b.use :label, class: 'control-label'
+
+    b.wrapper tag: 'div', class: 'controls' do |ba|
+      ba.use :input, class: 'form-control'
+      ba.use :error, wrap_with: { tag: 'span', class: 'help-block' }
+      ba.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
+    end
+  end
+end
