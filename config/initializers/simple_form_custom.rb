1df2fdb913bc4212ad56ff697ebed1c3564e1ae5
martent <marten@thavenius.se>
2014-05-04 22:12:05 +0200

    simple form w/ bs3

--- config/initializers/simple_form_custom.rb
+++ config/initializers/simple_form_custom.rb
@@ -1,5 +1,5 @@
 SimpleForm.setup do |config|
-  config.wrappers :basic, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
+  config.wrappers :basic, tag: 'div', class: 'form-group', error_class: 'warning' do |b|
     b.use :html5
     b.use :placeholder
     b.use :label, class: 'control-label'
@@ -9,5 +9,17 @@ SimpleForm.setup do |config|
       ba.use :error, wrap_with: { tag: 'span', class: 'help-block' }
       ba.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
     end
+  end
+
+  config.wrappers :addon_after, tag: 'div', class: "input-group", error_class: 'warning' do |b|
+    b.use :html5
+    b.use :placeholder
+    b.use :label
+    b.use :input
+    b.wrapper tag: 'span', class: 'input-group-btn' do |button|
+      button.use :input
+    end
+    b.use :error, wrap_with: { tag: 'p', class: 'help-block' }
+    b.use :hint,  wrap_with: { tag: 'span', class: 'help-block' }
   end
 end
