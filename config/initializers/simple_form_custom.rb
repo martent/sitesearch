c5b6b73b69281234de128f52607c3c5525b5fce8
martent <marten@thavenius.se>
2014-05-05 16:08:30 +0200

    BS3 fixes for error msgs

--- config/initializers/simple_form_custom.rb
+++ config/initializers/simple_form_custom.rb
@@ -1,9 +1,14 @@
+# Customized BS3 markup for symantic responsive forms
 SimpleForm.setup do |config|
+  config.default_wrapper = :basic
+  config.label_class = nil
+  config.error_notification_class = nil
+
   config.wrappers :basic, tag: 'div', class: 'form-group', error_class: 'warning' do |b|
     b.use :html5
     b.use :placeholder
     b.wrapper tag: 'span', class: 'control-label' do |label|
-      b.use :label, class: nil
+      label.use :label
     end
     b.wrapper tag: 'div', class: 'controls' do |ba|
       ba.use :input, class: 'form-control'
@@ -16,11 +21,12 @@ SimpleForm.setup do |config|
     b.use :html5
     b.use :placeholder
     b.wrapper tag: 'span', class: 'control-label' do |label|
-      b.use :label, class: nil
+      label.use :label
     end
     b.wrapper tag: 'div', class: 'controls input-group' do |ba|
       ba.use :input, class: 'form-control'
-      ba.use :error, wrap_with: { tag: 'span', class: 'help-block' }
+      # Breaks BS3 input-group
+      # ba.use :error, wrap_with: { tag: 'span', class: 'help-block' }
       ba.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
     end
   end
