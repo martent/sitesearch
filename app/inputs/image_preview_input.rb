6e17655ce7b6a8df55bc66000a2a6b75fe8f561a
martent <marten@thavenius.se>
2014-05-05 21:43:51 +0200

    Fixed deprecation warning

--- app/inputs/image_preview_input.rb
+++ app/inputs/image_preview_input.rb
@@ -1,5 +1,5 @@
 class ImagePreviewInput < SimpleForm::Inputs::FileInput
-  def input
+  def input(wrapper_options)
     # :preview_version is a custom attribute from :input_html hash, so you can pick custom sizes
     version = input_html_options.delete(:preview_version)
     out = '' # the output string we're going to build
@@ -12,6 +12,6 @@ class ImagePreviewInput < SimpleForm::Inputs::FileInput
     # allow multiple submissions without losing the tmp version
     out << @builder.hidden_field("#{attribute_name}_cache").html_safe
     # append file input. it will work accordingly with your simple_form wrappers
-    (out << @builder.file_field(attribute_name, input_html_options)).html_safe
+    (out << @builder.file_field(attribute_name, merge_wrapper_options(input_html_options, wrapper_options))).html_safe
   end
 end
