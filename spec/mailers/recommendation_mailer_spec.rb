8d1d891a1c8eaf3008997042525802ccab388155
martent <marten@thavenius.se>
2015-03-25 11:36:25 +0100

    Added test case for link checkers mailer

---
+++ spec/mailers/recommendation_mailer_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe RecommendationMailer do
+  after do
+    Recommendation.destroy_all
+  end
+
+  describe ':check_links' do
+    let(:mail) {
+      RecommendationMailer.check_links(
+        [message: 'failed', recommendation: create(:recommendation)]
+      )
+    }
+
+    it 'renders the headers' do
+      expect(mail.subject).to eq('Länkkontroll')
+      expect(mail.from.first).to match('noreply@malmo.se')
+    end
+
+    it 'renders the body' do
+      expect(mail.body.encoded).to match('Hej!')
+    end
+  end
+end
