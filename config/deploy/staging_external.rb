1719373176f44f7de14ae00bed630f33ad5c145f
martent <marten@thavenius.se>
2015-04-13 17:05:46 +0200

    Deleted staging cap

--- config/deploy/staging_external.rb
+++
@@ -1,9 +0,0 @@
-set :rails_env, :staging
-set :audience, "external"
-set :stage, :staging_external
-
-set :branch, "2.1-duo"
-
-role :app, %w{deployer@}
-role :web, %w{deployer@}
-role :db,  %w{deployer@}
