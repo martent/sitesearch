df06ea45c328a6155fd188e1e79248c851a24638
martent <marten@thavenius.se>
2015-08-26 14:28:34 +0200

    test-b

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,12 +1,14 @@
 (function() {
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  jQuery(function() {
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-1659147-1', {
-    'userId': 'test_user',
-    'cookieDomain': 'auto'
+    ga('create', 'UA-19475064-1', {
+      'userId': 'test_user',
+      'cookieDomain': 'auto'
+    });
+    ga('send', 'pageview');
   });
-  ga('send', 'pageview');
 }).call(this);
