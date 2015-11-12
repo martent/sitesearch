4528b8fa2122f02581fd65a586502eea469b479e
martent <marten@thavenius.se>
2015-11-12 16:45:38 +0100

    Changed server adress

--- config/deploy/production_external.rb
+++ config/deploy/production_external.rb
@@ -4,6 +4,6 @@ set :stage, :production_external
 
 set :branch, "master"
 
-role :app, %w{app_runner@161.52.13.46}
-role :web, %w{app_runner@161.52.13.46}
-role :db,  %w{app_runner@161.52.13.46}
+role :app, %w{app_runner@srvubuwebhost25.malmo.se}
+role :web, %w{app_runner@srvubuwebhost25.malmo.se}
+role :db,  %w{app_runner@srvubuwebhost25.malmo.se}
