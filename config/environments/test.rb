59d0a919344d03c77f695469fdbd75600c94eb2f
martent <marten@thavenius.se>
2016-11-09 16:36:50 +0100

    Updated config to current rails

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -13,7 +13,7 @@ Rails.application.configure do
   config.eager_load = false
 
   # Configure static asset server for tests with Cache-Control for performance.
-  config.serve_static_assets  = true
+  config.serve_static_files  = true
   config.static_cache_control = 'public, max-age=3600'
 
   # Show full error reports and disable caching.
@@ -25,6 +25,7 @@ Rails.application.configure do
 
   # Raise exceptions instead of rendering exception templates.
   config.action_dispatch.show_exceptions = false
+  config.active_record.raise_in_transactional_callbacks = true
 
   # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
